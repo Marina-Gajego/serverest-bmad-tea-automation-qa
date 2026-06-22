54b4371ffed070ad1d13763d6cac153a9795ad29
Marina Gajego <gajego.marina@gmail.com>
2026-06-22 20:34:30 -0300

    Iniciando CI/CD

--- src/test/java/br/com/marina/qa/runner/RunnerTest.java
+++ src/test/java/br/com/marina/qa/runner/RunnerTest.java
@@ -7,7 +7,6 @@ import org.junit.platform.suite.api.Suite;
 
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
-import static io.cucumber.junit.platform.engine.Constants.FILTER_TAGS_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
@@ -22,10 +21,6 @@ import static io.cucumber.junit.platform.engine.Constants.FILTER_TAGS_PROPERTY_N
                 "html:target/cucumber-report.html, " +
                 "json:target/cucumber-report.json, " +
                 "timeline:target/timeline"
-)
-@ConfigurationParameter(
-        key = FILTER_TAGS_PROPERTY_NAME,
-        value = "@regression"
 )
 public class RunnerTest {
 }
